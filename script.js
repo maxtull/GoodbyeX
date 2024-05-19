a1d9a3c2a409f65424bd6de9ffc6d128e04ce6aa
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2024-05-19 15:00:30 +0700

    fix: update selectors for premium button and tweet button

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Verified Tab
-    waitForSelector('#react-root > div > div > div.css-175oi2r.r-13qz1uu.r-417010.r-18u37iz > header > div > div > div > div:nth-child(1) > div.css-175oi2r.r-15zivkp.r-1bymd8e.r-13qz1uu.r-1awozwy > nav > a:nth-child(8) > div > div > svg').then((logo) => {
+    waitForSelector('a[href="/i/premium_sign_up"] > div > div:nth-child(1) > svg').then((logo) => {
         if (!logo) return;
         logo.innerHTML = '<svg viewBox="0 0 24 24" aria-hidden="true" class="r-1nao33i r-4qtqp9 r-yyyyoo r-lwhw9o r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-cnnz9e"> <g> <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path> </g> </svg>';
     });
@@ -39,13 +39,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Tweet Texts / Buttons
     new MutationObserver(() => {
         // New Tweet Button
-        const tweetButton = document.querySelector('#layers > div:nth-child(2) > div > div > div > div > div > div.css-175oi2r.r-1ny4l3l.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv.r-1habvwh > div.css-175oi2r.r-1wbh5a2.r-htvplk.r-1udh08x.r-1867qdf.r-rsyp9y.r-1pjcn9w.r-1potc6q > div > div > div > div:nth-child(3) > div.css-175oi2r.r-kemksi.r-1h8ys4a.r-1moyyf3.r-1pp923h > div:nth-child(1) > div > div > div > div.css-175oi2r.r-kemksi.r-jumn1c.r-xd6kpl.r-gtdqiz.r-ipm5af.r-184en5c > div:nth-child(2) > div > div > div > div > div > span > span');
+        const tweetButton = document.querySelector('#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div:nth-child(1) > div > div.css-175oi2r.r-kemksi.r-184en5c > div > div:nth-child(2) > div:nth-child(1) > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div > div > button > div > span > span');
         if (tweetButton && tweetButton.textContent != 'Tweet') {
             tweetButton.innerHTML = 'Tweet';
         }
 
         // Tweet Sidebar Button
-        const tweetSidebarButton = document.querySelector('#react-root > div > div > div.css-175oi2r.r-13qz1uu.r-417010.r-18u37iz > header > div > div > div > div.css-175oi2r.r-1habvwh > div.css-175oi2r.r-e7q0ms.r-1r5su4o > a > div > span > div > div > span > span')
+        const tweetSidebarButton = document.querySelector('#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > header > div > div > div > div:nth-child(1) > div.css-175oi2r.r-e7q0ms.r-12kyg2d > a > div > span > div > div > span > span')
         if (tweetSidebarButton && tweetSidebarButton.textContent != 'Tweet') {
             tweetSidebarButton.innerHTML = 'Tweet';
         }
